3e26c8c5c2a6c6be86d2d14139bb932e93f96a90
haZuny <gkwns5791@naver.com>
2022-05-16 01:35:11 +0900

    운영체제 모니터링 과제

---
+++ Java/Operating System/src/JavaMonitor2/BounedeBuffer.java
@@ -0,0 +1,93 @@
+package JavaMonitor2;
+
+public class BounedeBuffer<E> {
+	private static final int BUFFER_SIZE = 5;
+
+	private int count, in, out;
+	private E[] buffer;
+
+	public BounedeBuffer() {
+		count = 0;
+		in = 0;
+		out = 0;
+		buffer = (E[]) new Object[BUFFER_SIZE];
+	}
+
+	public synchronized void insert(E item) {
+		while (count == BUFFER_SIZE) {
+			try {
+				wait();
+			} catch (InterruptedException ie) {
+			}
+		}
+
+		buffer[in] = item;
+		in = (in + 1) % BUFFER_SIZE;
+		count++;
+
+		notify();
+		System.out.println("생산: " + item);
+	}
+
+	public synchronized E remove() {
+		E item;
+
+		while (count == 0) {
+			try {
+				wait();
+			} catch (InterruptedException ie) {
+			}
+		}
+
+		item = buffer[out];
+		out = (out + 1) % BUFFER_SIZE;
+		count--;
+
+		notify();
+		System.out.println("\t\t소비: " + item);
+
+		return item;
+	}
+
+	public static void main(String[] args) {
+		BounedeBuffer<Integer> buf = new BounedeBuffer<>();
+		Producer prod = new Producer(buf);
+		Consumer cons = new Consumer(buf);
+
+		prod.start();
+		cons.start();
+	}
+
+}
+
+//생산자 스레드
+class Producer extends Thread {
+	BounedeBuffer<Integer> buffer;
+	int num;
+
+	// 생성자
+	Producer(BounedeBuffer<Integer> buf) {
+		this.buffer = buf;
+	}
+
+	public void run() {
+		for (int i = 0; i < 20; i++) {
+			buffer.insert((int) (Math.random() * 100));
+		}
+	}
+}
+
+// 소비자 스레드
+class Consumer extends Thread {
+	BounedeBuffer<Integer> buffer;
+
+	// 생성자
+	Consumer(BounedeBuffer<Integer> buf) {
+		this.buffer = buf;
+	}
+
+	public void run() {
+		for (int i = 0; i < 20; i++)
+			buffer.remove();
+	}
+}
